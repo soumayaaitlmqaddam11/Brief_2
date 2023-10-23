7c88b16be190fd1c62b894c83b36cd35294bb5a1
soumaya <soumiatinghir119@gmail.com>
2023-10-23 19:01:28 +0200

    contact page version 1

---
+++ script.js
@@ -0,0 +1,18 @@
+function openFunction(){
+document.getElementById('sidebar').style.width=' 200px'
+}
+function closeFunction(){
+    document.getElementById('sidebar').style.width='0px'
+}
+//  var  MyCard=document.getElementById('card-shopp');
+//  console.log(MyCard);
+//  function afficher(){
+//     var  MyCard = document.getElementById("card-shopp");
+//     if(MyCard.classList.contains("hide")){
+//         MyCard.classList.remove("hide")
+//     }
+//     else{
+//         MyCard.classList.add("hide")
+//     }
+//  }
+ 
