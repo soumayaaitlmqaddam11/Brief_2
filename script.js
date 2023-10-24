2f7e5227eca0c8e846a3c96a957e62f0b2fcdaa1
soumaya <soumiatinghir119@gmail.com>
2023-10-24 19:54:50 +0200

    add a sidbar

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
+
+ 
 function openFunction(){
 document.getElementById('sidebar').style.width=' 200px'
 }
 function closeFunction(){
     document.getElementById('sidebar').style.width='0px'
 }
-//  var  MyCard=document.getElementById('card-shopp');
-//  console.log(MyCard);
-//  function afficher(){
-//     var  MyCard = document.getElementById("card-shopp");
-//     if(MyCard.classList.contains("hide")){
-//         MyCard.classList.remove("hide")
-//     }
-//     else{
-//         MyCard.classList.add("hide")
-//     }
-//  }
- 
